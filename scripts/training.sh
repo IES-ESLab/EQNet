61d73b98ca6ccd360122dc9180d87ace773c58c0
wanghaoyu <justgoback@126.com>
2025-07-05 03:29:19 -0700

    update training.sh

--- scripts/training.sh
+++ scripts/training.sh
@@ -3,63 +3,83 @@ set -x
 
 torchrun --master_port 29510  --nproc_per_node 4 train.py --model phasenet  --backbone xunet \
     --batch-size 128 --workers 16  --lr 0.0003 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_xunet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_xunet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_xunet
 
 torchrun --master_port 29511  --nproc_per_node 4 train.py --model phasenet  --backbone unet \
     --batch-size 128 --workers 16  --lr 0.001 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_unet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_unet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_unet
 
 torchrun --master_port 29512  --nproc_per_node 4 train.py --model phasenet_tf  --backbone xunet \
     --batch-size 128 --workers 16  --lr 0.0003 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_tf_xunet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_tf_xunet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_tf_xunet
 
 torchrun --master_port 29513  --nproc_per_node 4 train.py --model phasenet_tf  --backbone unet \
     --batch-size 128 --workers 16  --lr 0.001 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_tf_unet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_tf_unet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_tf_unet
 
 torchrun --master_port 29514  --nproc_per_node 4 train.py --model phasenet_plus  --backbone xunet \
     --batch-size 128 --workers 16  --lr 0.0003 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_plus_xunet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_plus_xunet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_plus_xunet
 
 torchrun --master_port 29515  --nproc_per_node 4 train.py --model phasenet_plus  --backbone unet \
     --batch-size 128 --workers 16  --lr 0.001 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_plus_unet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_plus_unet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_plus_unet
 
 torchrun --master_port 29517  --nproc_per_node 4 train.py --model phasenet_tf_plus  --backbone xunet \
     --batch-size 128 --workers 16  --lr 0.0003 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_tf_plus_xunet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_tf_plus_xunet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_tf_plus_xunet
 
 torchrun --master_port 29516  --nproc_per_node 4 train.py --model phasenet_tf_plus  --backbone unet \
     --batch-size 128 --workers 16  --lr 0.001 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_tf_plus_unet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_tf_plus_unet 
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_tf_plus_unet 
 
 torchrun --master_port 29518  --nproc_per_node 4 train.py --model phasenet_prompt  --backbone xunet \
     --workers 16 --batch-size 16 --lr 0.0003 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_prompt_xunet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_prompt_xunet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_prompt_xunet
 
 torchrun --master_port 29519  --nproc_per_node 4 train.py --model phasenet_prompt  --backbone unet \
     --workers 16 --batch-size 16 --lr 0.001 \
-    --hdf5-file /global/home/users/zhuwq0/scratch/CEED/quakeflow_nc/waveform_test.h5 \
+    --hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
+    --test-hdf5-file /global/scratch/users/haoyuwang/quakeflow_dataset/NC/quakeflow_nc/waveform_train.h5 \
     --output-dir phasenet_prompt_unet \
-    --wandb --wandb-project phasenet --wandb-name phasenet_prompt_unet
+    --stack-event --stack-noise --flip-polarity --drop-channel \
+    --wandb --wandb-group ai4eps  --wandb-dir /pscratch/sd/w/whaoyu/tests_eqnet/ --wandb-project phasenet --wandb-name phasenet_prompt_unet
 
 DATA_PATH=/global/scratch/users/zhuwq0/quakeflow_das
 LABEL_PATH=/global/home/users/zhuwq0/scratch/EQNet/scripts/results/training_v0
