f6dddb74eee7a8b607259c22833fe80ff76a26a0
Weiqiang Zhu <zhuwq0@gmail.com>
2025-07-14 22:19:44 -0700

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="EQNet",
-    version="0.1.5",
+    version="0.1.7",
     long_description="EQNet: Neural Network Models for Earthquakes",
     long_description_content_type="text/markdown",
-    packages=["eqnet"],
+    packages=find_packages(),
     install_requires=["torch", "torchvision", "h5py", "matplotlib", "pandas"],
 )
